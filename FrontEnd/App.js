28086709bdfb0c917416c2c19c529c03b88e17f6
Doğukan Kutluay <dogukan.kutluay@renticar.com>
2022-12-11 19:35:42 +0300

    React native create

---
+++ FrontEnd/App.js
@@ -0,0 +1,10 @@
+const {View, Text} = require('react-native');
+import React from 'react';
+const App = () => {
+  return (
+    <View>
+      <Text>Campvill</Text>
+    </View>
+  );
+};
+export default App;
