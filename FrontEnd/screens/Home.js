55be112639bab806ee5e25d89cc08967ff6f8716
Berkay3734 <berkay.gameofbilisim@gmail.com>
2023-01-10 18:33:58 +0300

    scrollView yapıldi

--- FrontEnd/screens/Home.js
+++ FrontEnd/screens/Home.js
@@ -1,13 +1,31 @@
 import React from "react";
-import {View, Text, Image, TouchableOpacity, ScrollView} from 'react-native';
+import {View, Text, Image,ScrollView} from 'react-native';
 import homeStyle from "../styles/screen/home";
+import test from "../assets/img/onboardingPage/2.png";
 
-const Home = ({navigation}) => {
+const Home = () => {
   return (
     <View style={homeStyle.container}>
-       {/* <ScrollView style={homeStyle.story}>
-        
-       </ScrollView> */}
+      <Text>buraya bir şeyler gelebilir</Text>
+       <ScrollView style={homeStyle.story}
+       horizontal={true}
+       showsHorizontalScrollIndicator={false}>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+          <Image style={homeStyle.resim} source={test}></Image>
+       </ScrollView>
     </View>
   );
 };
--- FrontEnd/styles/screen/home.js
+++ FrontEnd/styles/screen/home.js
@@ -10,5 +10,13 @@ const homeStyle = StyleSheet.create({
     borderWidth:1,
     borderColor:'blue',
   },
+  resim:{
+    width:60,
+    height:60,
+    borderRadius:100,
+    borderWidth:1,
+    borderColor:'red',
+    marginLeft:10,
+  }
 });
 export default homeStyle;
