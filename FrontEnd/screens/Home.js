3159eceb488f6d297fd3287da046181e31520b11
Berkay3734 <berkay.gameofbilisim@gmail.com>
2023-01-11 18:19:08 +0300

    HomeScreen'e ekleme yapıldı

--- FrontEnd/screens/Home.js
+++ FrontEnd/screens/Home.js
@@ -1,30 +1,103 @@
 import React from "react";
-import {View, Text, Image,ScrollView} from 'react-native';
+import {View, Text, ImageBackground,ScrollView,Image, Touchable, TouchableOpacity} from 'react-native';
 import homeStyle from "../styles/screen/home";
 import test from "../assets/img/onboardingPage/2.png";
-
+import nokta from "../assets/img/onboardingPage/Dot.png";
 const Home = () => {
   return (
     <View style={homeStyle.container}>
-      <Text>buraya bir şeyler gelebilir</Text>
        <ScrollView style={homeStyle.story}
        horizontal={true}
        showsHorizontalScrollIndicator={false}>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
-          <Image style={homeStyle.resim} source={test}></Image>
+          <ImageBackground style={homeStyle.resim} source={test}>
+            <View><Text style={homeStyle.nickname}>NickName</Text></View>
+          </ImageBackground>
+          <ImageBackground style={homeStyle.resim} source={test}>
+          <View><Text style={homeStyle.nickname}>NickName</Text></View>
+          </ImageBackground>
+          <ImageBackground style={homeStyle.resim} source={test}>
+          <View><Text style={homeStyle.nickname}>NickName</Text></View>
+          </ImageBackground>
+          <ImageBackground style={homeStyle.resim} source={test}>
+          <View><Text style={homeStyle.nickname}>NickName</Text></View>
+          </ImageBackground>
+          <ImageBackground style={homeStyle.resim} source={test}>
+          <View><Text style={homeStyle.nickname}>NickName</Text></View>
+          </ImageBackground>
+          <ImageBackground style={homeStyle.resim} source={test}>
+          <View><Text style={homeStyle.nickname}>NickName</Text></View>
+          </ImageBackground>
+          <ImageBackground style={homeStyle.resim} source={test}>
+          <View><Text style={homeStyle.nickname}>NickName</Text></View>
+          </ImageBackground>
+          <ImageBackground style={homeStyle.resim} source={test}>
+          <View><Text style={homeStyle.nickname}>NickName</Text></View>
+          </ImageBackground>
+          <ImageBackground style={homeStyle.resim} source={test}>
+          <View><Text style={homeStyle.nickname}>NickName</Text></View>
+          </ImageBackground>
+       </ScrollView>
+       <ScrollView showsVerticalScrollIndicator={false}>
+       <View style={homeStyle.shareContainer}>
+          <View style={homeStyle.share}>
+          <Image style={homeStyle.miniResim} source={test}></Image>
+          <Text>Your NickName</Text>
+          <TouchableOpacity style={{position:'absolute',right:0}}>
+          <Image source={nokta} style={{
+            width:30,height:30
+            }}></Image>
+            </TouchableOpacity>
+          </View>
+          <Image source={test} style={{width:'100%',height:400}}></Image>
+       </View>
+       <View style={homeStyle.shareContainer}>
+          <View style={homeStyle.share}>
+          <Image style={homeStyle.miniResim} source={test}></Image>
+          <Text>Your NickName</Text>
+          <TouchableOpacity style={{position:'absolute',right:0}}>
+          <Image source={nokta} style={{
+            width:30,height:30
+            }}></Image>
+            </TouchableOpacity>
+          </View>
+          <Image source={test} style={{width:'100%',height:400}}></Image>
+       </View>
+       <View style={homeStyle.shareContainer}>
+          <View style={homeStyle.share}>
+          <Image style={homeStyle.miniResim} source={test}></Image>
+          <Text>Your NickName</Text>
+          <TouchableOpacity style={{position:'absolute',right:0}}>
+          <Image source={nokta} style={{
+            width:30,height:30
+            }}></Image>
+            </TouchableOpacity>
+          </View>
+          <Image source={test} style={{width:'100%',height:400}}></Image>
+       </View>
+       <View style={homeStyle.shareContainer}>
+          <View style={homeStyle.share}>
+          <Image style={homeStyle.miniResim} source={test}></Image>
+          <Text>Your NickName</Text>
+          <TouchableOpacity style={{position:'absolute',right:0}}>
+          <Image source={nokta} style={{
+            width:30,height:30
+            }}></Image>
+            </TouchableOpacity>
+          </View>
+          <Image source={test} style={{width:'100%',height:400}}></Image>
+       </View>
+       <View style={homeStyle.shareContainer}>
+          <View style={homeStyle.share}>
+          <Image style={homeStyle.miniResim} source={test}></Image>
+          <Text>Your NickName</Text>
+          <TouchableOpacity style={{position:'absolute',right:0}}>
+          <Image source={nokta} style={{
+            width:30,height:30
+            }}></Image>
+            </TouchableOpacity>
+          </View>
+          <Image source={test} style={{width:'100%',height:400}}></Image>
+       </View>
        </ScrollView>
     </View>
   );
--- FrontEnd/styles/screen/home.js
+++ FrontEnd/styles/screen/home.js
@@ -9,6 +9,7 @@ const homeStyle = StyleSheet.create({
   story: {
     borderWidth:1,
     borderColor:'blue',
+    height:'15%',
   },
   resim:{
     width:60,
@@ -17,6 +18,32 @@ const homeStyle = StyleSheet.create({
     borderWidth:1,
     borderColor:'red',
     marginLeft:10,
+  },
+  miniResim:{
+    width:40,
+    height:40,
+    borderRadius:100,
+    borderWidth:1,
+    borderColor:'red',
+    marginLeft:10,
+  },
+  share:{
+    display:'flex',
+    flexDirection:'row',
+    alignItems:'center',
+    borderWidth:1,
+    borderColor:'pink',
+  },
+  shareContainer:{
+    marginTop:10,
+    borderWidth:1,
+    borderColor:'yellow',
+  },
+  nickname:{
+    display:'flex',
+    position:'absolute',
+    bottom:-75,
+    fontSize:11,
   }
 });
 export default homeStyle;
